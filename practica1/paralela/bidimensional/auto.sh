009e4f2e2c7ec57dc469fb028bf353ecf76874dc
marlenelis <mvas.calero@gmail.com>
2016-05-18 08:58:58 +0200

    añadido datos

--- practica1/paralela/bidimensional/auto.sh
+++ practica1/paralela/bidimensional/auto.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 #FICH=salidaSIN.txt
-FICH=salida2.txt
-echo "*******400******** ">> $FICH 
+FICH=salida.txt
+N=1;
 LIMIT=10
+
+echo "*******400******** ">> $FICH 
+
 for ((a=0; a<LIMIT; a++))
 do
- mpirun  -np 4 ./floyd ../input/input400 >> $FICH 
+ mpirun  -np $N  ./floyd ../input/input400 >> $FICH 
 done
 
 echo " *****800******\n ">>$FICH 
 
 for ((a=0; a<LIMIT; a++))
 do
- mpirun  -np 4  ./floyd ../input/input800 >> $FICH 
+ mpirun  -np $N   ./floyd ../input/input800 >> $FICH 
 done
 
 echo " *****1200******\n ">>$FICH 
 
 for ((a=0; a<LIMIT; a++))
 do
- mpirun  -np 4 ./floyd ../input/input1200 >> $FICH 
+ mpirun  -np $N  ./floyd ../input/input1200 >> $FICH 
 done
 
 echo " *****1600******\n ">>$FICH 
 
 for ((a=0; a<LIMIT; a++))
 do
- mpirun  -np 4 ./floyd ../input/input1600 >> $FICH 
+ mpirun  -np $N ./floyd ../input/input1600 >> $FICH 
 done
--- practica1/paralela/unidimensional/auto.sh
+++ practica1/paralela/unidimensional/auto.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 #FICH=salidaSIN.txt
-FICH=salida2.txt
+FICH=salida1.txt
 echo "*******400******** ">> $FICH 
 LIMIT=10
 for ((a=0; a<LIMIT; a++))
 do
- mpirun  -np 4 ./floyd ../input/input400 >> $FICH 
+ mpirun  -np 1 ./floyd ../input/input400 >> $FICH 
 done
 
 echo " *****800******\n ">>$FICH 
 
 for ((a=0; a<LIMIT; a++))
 do
- mpirun  -np 4  ./floyd ../input/input800 >> $FICH 
+ mpirun  -np 1  ./floyd ../input/input800 >> $FICH 
 done
 
 echo " *****1200******\n ">>$FICH 
 
 for ((a=0; a<LIMIT; a++))
 do
- mpirun  -np 4 ./floyd ../input/input1200 >> $FICH 
+ mpirun  -np 1 ./floyd ../input/input1200 >> $FICH 
 done
 
 echo " *****1600******\n ">>$FICH 
 
 for ((a=0; a<LIMIT; a++))
 do
- mpirun  -np 4 ./floyd ../input/input1600 >> $FICH 
+ mpirun  -np 1 ./floyd ../input/input1600 >> $FICH 
 done
